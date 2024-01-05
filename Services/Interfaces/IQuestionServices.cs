929ed267907069e7c821d9653ea5e26a3674645d
qyhngan <nguyenquynhngan4143@gmail.com>
2024-01-05 20:31:08 +0700

    add IQuestion service

--- Services/Interfaces/IQuestionServices.cs
+++ Services/Interfaces/IQuestionServices.cs
@@ -1,6 +1,17 @@
-﻿namespace Services.Interfaces
+﻿using Services.ViewModels;
+
+namespace Services.Interfaces
 {
     public interface IQuestionServices
     {
+        Task<bool> AddQuestions(QuestionCreate questionCreate, Guid currentUser);
+        Task<bool> DeleteQuestion(Guid questionId, Guid currentUser);
+        Task<IEnumerable<QuestionViewModels>> GetAllQuestion();
+        Task<QuestionViewModels> GetQuestionByQuestionId(Guid questionId);
+        Task<bool> UpdateQuestion(QuestionUpdate questionUpdate, Guid currentUser);
+
+
+
+
     }
 }
