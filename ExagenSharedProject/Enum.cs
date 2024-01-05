04d14af71b931d10bd48e8fb9a4728ccbc08ea37
qyhngan <nguyenquynhngan4143@gmail.com>
2024-01-05 20:43:41 +0700

    Add question controller and sharedproject

---
+++ ExagenSharedProject/Enum.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExagenSharedProject
+{
+    public static class Enum
+    {
+
+        //Create Status Enum for Question
+        public static Dictionary<int, string> QuestionStatus = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Mục đích cá nhân"),
+                new KeyValuePair<int, string>(1, "Để bán")
+            });
+
+        //Create Status Enum for User
+        public static Dictionary<int, string> UserStatus = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Ngưng hoạt động"),
+                new KeyValuePair<int, string>(1, "Đang hoạt động"),
+                new KeyValuePair<int, string>(2, "Đang chờ xác thực"),
+                new KeyValuePair<int, string>(3, "Đã xác thực")
+            });
+
+        //Create Status Enum for Test
+        public static Dictionary<int, string> TestStatus = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Bản nháp"),
+                new KeyValuePair<int, string>(1, "Chính thức")
+            });
+
+        //Create Status Enum for StudentClass
+        public static Dictionary<int, string> StudentClassStatus = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Ngưng hoạt động"),
+                new KeyValuePair<int, string>(1, "Đang hoạt động")
+            });
+
+        //Create Option Set for Grade of Subject Section
+        public static Dictionary<int, string> Grade = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>( 0, "Khác"),
+                new KeyValuePair<int, string>( 1, "Lớp 1"),
+                new KeyValuePair<int, string>( 2, "Lớp 2"),
+                new KeyValuePair<int, string>( 3, "Lớp 3"),
+                new KeyValuePair<int, string>( 4, "Lớp 4"),
+                new KeyValuePair<int, string>( 5, "Lớp 5"),
+                new KeyValuePair<int, string>( 6, "Lớp 6"),
+                new KeyValuePair<int, string>( 7, "Lớp 7"),
+                new KeyValuePair<int, string>( 8, "Lớp 8"),
+                new KeyValuePair<int, string>( 9, "Lớp 9"),
+                new KeyValuePair<int, string>(10, "Lớp 10"),
+                new KeyValuePair<int, string>(11, "Lớp 11"),
+                new KeyValuePair<int, string>(12, "Lớp 12")
+            });
+
+        //Create Option Set for Subject of Subject Section
+        public static Dictionary<int, string> Subject = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Khác"),
+                new KeyValuePair<int, string>(1, "Toán"),
+                new KeyValuePair<int, string>(2, "Ngữ văn/ Tiếng Việt"),
+                new KeyValuePair<int, string>(3, "Tiếng Anh"),
+                new KeyValuePair<int, string>(4, "Vật lý"),
+                new KeyValuePair<int, string>(5, "Hóa học"),
+                new KeyValuePair<int, string>(6, "Sinh học"),
+                new KeyValuePair<int, string>(7, "Lịch sử"),
+                new KeyValuePair<int, string>(8, "Địa lý"),
+                new KeyValuePair<int, string>(9, "Giáo dục công dân"),
+                new KeyValuePair<int, string>(10, "Công nghệ"),
+                new KeyValuePair<int, string>(11, "Tin học"),
+                new KeyValuePair<int, string>(12, "Giáo dục quốc phòng")
+            });
+
+        //Create Option Set for Type of Share
+        public static Dictionary<int, string> Type = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Tính điểm"),
+                new KeyValuePair<int, string>(1, "Miễn phí")
+            });
+
+        //Create Option Set for PermissionType of Share
+        public static Dictionary<int, string> PermissionType = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Đọc"),
+                new KeyValuePair<int, string>(1, "Đọc, Sửa"),
+                new KeyValuePair<int, string>(2, "Đọc, Sửa, Share"),
+                new KeyValuePair<int, string>(3, "Đọc, Sửa, Share, Xóa")
+            });
+
+        //Create Option Set for ShareLevel of Share
+        public static Dictionary<int, string> ShareLevel = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Người dùng"),
+                new KeyValuePair<int, string>(1, "Trường"),
+                new KeyValuePair<int, string>(2, "Cộng đồng")
+            });
+
+        //Create Option Set for Difficulty of Question
+        public static Dictionary<int, string> Difficulty = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Nhận biết"),
+                new KeyValuePair<int, string>(1, "Thông hiểu"),
+                new KeyValuePair<int, string>(2, "Vận dụng"),
+                new KeyValuePair<int, string>(3, "Vận dụng cao")
+            });
+
+        //Create Option Set for UserType of User
+        public static Dictionary<int, string> PositionID = new Dictionary<int, string>(
+            new KeyValuePair<int, string>[]
+            {
+                new KeyValuePair<int, string>(0, "Admin hệ thống"),
+                new KeyValuePair<int, string>(1, "Giáo viên"),
+                new KeyValuePair<int, string>(2, "Admin trường"),
+                new KeyValuePair<int, string>(3, "Chuyên gia")
+            });
+    }
+}
+
+
+
---
+++ WebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Services.ViewModels;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionServices _questionServices;
+        private readonly IUserServices _userServices;
+
+        [HttpPost("AddQuestion")]
+        public async Task<IActionResult> AddQuestion([FromBody] QuestionCreate questionCreate)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var currentUser = await _userServices.GetCurrentUser();
+
+                var result = await _questionServices.AddQuestions(questionCreate, currentUser);
+
+                if (!result)
+                {
+                    return BadRequest(new
+                    {
+                        Message = "Add Question thất bại"
+                    });
+                }
+
+                return Ok("Đã thêm câu hỏi mới");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+
+        [HttpGet("GetAllQuestion")]
+        public async Task<IActionResult> GetAllQuestion()
+        {
+            try
+            {
+                var questions = await _questionServices.GetAllQuestion();
+
+                return Ok(questions);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("GetQuestionByQuestionId/{questionId}")]
+        public async Task<IActionResult> GetQuestionByQuestionId(Guid questionId)
+        {
+            try
+            {
+                var question = await _questionServices.GetQuestionByQuestionId(questionId);
+
+                return Ok(question);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpPut("UpdateQuestion")]
+        public async Task<IActionResult> UpdateQuestion([FromBody] QuestionUpdate questionUpdate)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var currentUser = await _userServices.GetCurrentUser();
+
+                var result = await _questionServices.UpdateQuestion(questionUpdate, currentUser);
+
+                if (!result)
+                {
+                    return BadRequest(new
+                    {
+                        Message = "Update Question thất bại"
+                    });
+                }
+
+                return Ok("Cập nhật thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpDelete("DeleteQuestion")]
+        public async Task<IActionResult> DeleteQuestion(Guid questionId)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var currentUser = await _userServices.GetCurrentUser();
+
+                var result = await _questionServices.DeleteQuestion(questionId, currentUser);
+
+                if (!result)
+                {
+                    return BadRequest(new
+                    {
+                        Message = "Delete Question thất bại"
+                    });
+                }
+
+                return Ok("Xóa thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+    }
+
+
+}
