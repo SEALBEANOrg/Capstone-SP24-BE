b23be2f14c22662d43e137f920cc391a1c0ff2aa
SEAL.BEAN <xuhet2017@gmail.com>
2024-02-17 13:33:02 +0700

    Import Students from Excel file

---
+++ DucumentProcessing/ImportStudents.cs
@@ -0,0 +1,30 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DucumentProcessing
+{
+    public static class ImportStudents
+    {
+        //Import students from a Excel file
+        public static List<string> Import(string filePath)
+        {
+            var list = new List<string>();
+            var workbook = new Workbook();
+
+            workbook.LoadFromFile(filePath);
+
+            Worksheet sheet = workbook.Worksheets[0]; // get the first worksheet
+
+            for (int row = 1; row <= sheet.LastRow; row++)
+            {
+                list.Add(sheet.Range[row, 1].Text); // read the value in the first column of the current row
+            }
+
+            return list;
+        }
+    }
+}
--- TestClass/Program.cs
+++ TestClass/Program.cs
@@ -3,8 +3,10 @@ using System.Text;
 
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-ImportQuestionSet.ImportQuestions("D:\\CapstoneSP24\\backend\\DucumentProcessing\\LichSu12.docx");
+//ImportQuestionSet.ImportQuestions("D:\\CapstoneSP24\\backend\\DucumentProcessing\\LichSu12.docx");
 //ImportQuestionSet.ImportQuestions("D:\\CapstoneSP24\\backend\\DucumentProcessing\\LichSu12CoDapAn.docx");
+
+var result = ImportStudents.Import("D:\\CapstoneSP24\\backend\\DucumentProcessing\\DSLop.xlsx");
 
 //File.WriteAllText("output.txt", result, Encoding.Unicode);
 //Console.WriteLine(result);
