abe13fe4b2069c8a04dc0bfbc80fd68b93e6d279
SEAL.BEAN <xuhet2017@gmail.com>
2024-02-22 21:58:18 +0700

    Change config database

--- Repositories/Models/ExagenContext.cs
+++ Repositories/Models/ExagenContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace Repositories.Models
 {
@@ -31,12 +33,21 @@ namespace Repositories.Models
         public virtual DbSet<SubjectSection> SubjectSections { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
 
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var strConn = config["ConnectionStrings:Exagen"];
+            return strConn;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =sealbean.ddns.net,5555; database = Exagen;uid=sa;pwd=741753963741753963; TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
         }
 
