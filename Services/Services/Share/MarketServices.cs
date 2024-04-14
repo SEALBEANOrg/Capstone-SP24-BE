7b5dcb57753a8699d974b5ba5af770327b85586a
qyhngan <nguyenquynhngan4143@gmail.com>
2024-04-14 14:10:24 +0700

    update feature get price of questionset in market

--- Services/Services/Share/MarketServices.cs
+++ Services/Services/Share/MarketServices.cs
@@ -246,12 +246,13 @@ namespace Services.Services.Share
 
                 foreach (var s in shares)
                 {
+                    var questions = await _unitOfWork.QuestionRepo.FindListByField(question => question.QuestionSetId == s.QuestionSetId);
                     var config = new SetConfig
                     {
-                        NB = s.QuestionSet.Questions.Count(c => c.Difficulty == 0),
-                        TH = s.QuestionSet.Questions.Count(c => c.Difficulty == 1),
-                        VDT = s.QuestionSet.Questions.Count(c => c.Difficulty == 2),
-                        VDC = s.QuestionSet.Questions.Count(c => c.Difficulty == 3),
+                        NB = questions.Count(c => c.Difficulty == 0),
+                        TH = questions.Count(c => c.Difficulty == 1),
+                        VDT = questions.Count(c => c.Difficulty == 2),
+                        VDC = questions.Count(c => c.Difficulty == 3),
                     };
 
                     var shareInMarket = _mapper.Map<ShareInMarket>(s);
